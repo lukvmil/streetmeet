8ee4e1ee120fff91dedfe9d9e1b24b7da5b9fa19
Aeolus1364 <millerluke1364@gmail.com>
2022-10-01 14:20:13 -0400

    added starting timeout for person document

--- app/resources/person.py
+++ app/resources/person.py
@@ -1,5 +1,6 @@
 from flask import request
 from flask_restful import Resource, abort
+from datetime import datetime, timedelta
 from app.models import PersonModel
 from app import shared
 
@@ -7,8 +8,9 @@ class CreatePerson(Resource):
     def post(self):
         data = request.get_json()
         p = PersonModel(**data)
+        p.timeout = datetime.now() + timedelta(minutes=1)
         p.save()
-        
+
 # class KeepAlive(Resource):
 #     def post(self):
         
