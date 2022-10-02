ff36e6a6f2479bca228c82cfa30c9f42a91c3776
Udbhav <udbhavs03@gmail.com>
2022-10-01 21:31:23 -0400

    change output config

--- client1/vue.config.js
+++ client1/vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
 
 module.exports = defineConfig({
+  outputDir: path.resolve(__dirname, "../static"),
   transpileDependencies: true,
   lintOnSave: false,
   devServer: {
