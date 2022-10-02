9266752fa640c4bea1a993fcb037808daaa7a325
lukvmil <millerluke1364@gmail.com>
2022-10-02 01:13:10 +0000

    aligning with front end, bug fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_restful import Api
+from flask_cors import CORS
 from mongoengine import connect
 from app.resources import CreateUser, KeepAlive, FindMatch
 
@@ -7,11 +8,12 @@ connect('streetmeet')
 
 main = Flask('StreetMeet')
 api  = Api(main)
+cors = CORS(main, origins=['https://lukvmil.github.io'])
 
 @main.route('/')
 def home():
     return "Hello, world!"
 
-api.add_resource(CreateUser, '/user', '/user/')
-api.add_resource(KeepAlive,'/user/<id>')
-api.add_resource(FindMatch, '/match/<id>')
+api.add_resource(CreateUser, '/api/user', '/user/')
+api.add_resource(KeepAlive,'/api/user/<id>')
+api.add_resource(FindMatch, '/api/match/<id>')
--- app/resources/match.py
+++ app/resources/match.py
@@ -6,16 +6,19 @@ class FindMatch(Resource):
 	def get(self, id):
 		if not UserModel.objects.count(): return []
 		d = UserModel.objects(pk=id).first()
+		if not d: abort(400, message="user not found")
 		matches = UserModel.objects(location__within_spherical_distance=[d.location, 5/3963.2])
 		valid_matches = []
 		for m in matches:
 			if datetime.now() > m.timeout:
 				m.delete()
 			else:
+				if m.id == id: continue
 				valid_matches.append({
 					'id': m.id,
 					'username': m.username,
-					'topic': m.topic
+					'topic': m.topic,
+					'peerId': m.peer_id
 				})
 
 		return valid_matches
